4ed48de3179b46d13a14133e2c7b5d8e2c644735
fluffy29 <hmajzoub12@gmail.com>
2025-04-19 23:10:41 +0200

    Add routing and page components for the application

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages
+import CreateProduct from "./pages/CreateProduct";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import SignUpPage from "./pages/SignUpPage";
+
+// Components
+import NavBar from "./components/Navbar";
+import FooterComp from "./components/FooterComp";
+
 const App = () => {
   return (
-    <div className="container d-flex flex-column justify-content-center align-items-center vh-100 w-100">
-      <h1 className="text-center">App</h1>
-    </div>
-  )
-}
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/create-product" element={<CreateProduct />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+        <FooterComp />
+      </Router>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ vite-project/src/components/FooterComp.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FooterComp = () => {
+  return (
+    <div>Product</div>
+  );
+};
+
+export default FooterComp;
